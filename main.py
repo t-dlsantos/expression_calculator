a142854c68ff675642adb5e6205f371fb4b2773c
Henry <hsanturiao@gmail.com>
2024-06-02 15:38:34 -0300

    feat: def init ExpressionAnalyzer

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import re
 from Arvore.BinaryTree import NodeTree
 from Pilha.Pilha import Pilha
+
+
 class ExpressionAnalyzer:
+    def __init__(self):
+        self.operators = set(['+', '-', '*', '/'])
+        self.priority = {'+':1, '-':1, '*':2, '/':2}
+    
